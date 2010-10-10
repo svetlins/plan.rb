5048405433f891efb4c0d601772c74269870e95e
svetlins <svetlin.s@gmail.com>
2010-10-10 21:03:02 +0300

    cleaned parsing tests a bit and added neg numbers

--- tests/01_parser.rb
+++ tests/01_parser.rb
@@ -8,11 +8,15 @@ class TestSchemeParser < Test::Unit::TestCase
     end
 
     def test_atoms
-        assert_equal(Scheme::parse("1"), 1)
+        assert_equal(0, Scheme::parse("0"))
+        assert_equal(1, Scheme::parse("1"))
+        assert_equal(42, Scheme::parse("42"))
+        assert_equal(-42, Scheme::parse("-42"))
+        assert_equal(69, Scheme::parse("+69"))
 
-        assert_equal(Scheme::parse("\"test\""), "test")
-        assert_equal(Scheme::parse("\"test+-*=,.[]()\""), "test+-*=,.[]()")
-        assert_equal(Scheme::parse("\"+-*=,.[]()\""), "+-*=,.[]()")
+        assert_equal("test", Scheme::parse("\"test\""))
+        assert_equal("test+-*=,.[]()", Scheme::parse("\"test+-*=,.[]()\""))
+        assert_equal("+-*=,.[]()", Scheme::parse("\"+-*=,.[]()\""))
     end
 
     def test_lists
@@ -30,23 +34,23 @@ class TestSchemeParser < Test::Unit::TestCase
 
 
     def test_symbols
-        assert_equal(Scheme::parse("(#t #f)").car , true)
-        assert_equal(Scheme::parse("(#t #f)").cdr.car , false)
+        assert_equal(true, Scheme::parse("(#t #f)").car)
+        assert_equal(false, Scheme::parse("(#t #f)").cdr.car)
 
         assert_equal(4, Scheme::parse("(4 symbol other 5)").car)
         assert_equal(:symbol, Scheme::parse("(symbol symbol)").car)
-        assert_equal(Scheme::parse("(quote abv)").cdr.car, :abv)
+        assert_equal(:abv, Scheme::parse("(quote abv)").cdr.car)
 
-        assert_equal(Scheme::parse("(null? nil)").car, :null?)
+        assert_equal(:null?, Scheme::parse("(null? nil)").car)
 
-        assert_equal(Scheme::parse('(a)').car, :a)
+        assert_equal(:a, Scheme::parse('(a)').car)
 
-        assert_equal(Scheme::parse("nil"), nil)
-        assert_equal(Scheme::parse("(cons 1 nil)").cdr.cdr.car, nil)
-        assert_equal(Scheme::parse("(cons 1 2)").car, :cons)
-        assert_equal(Scheme::parse("(cons 1 (cons 1 2))").cdr.cdr.car.car, :cons)
-        assert_equal(Scheme::parse("((cons 1  2) (cons 1 2))").cdr.car.car, :cons)
+        assert_equal(nil, Scheme::parse("nil"))
+        assert_equal(nil, Scheme::parse("(cons 1 nil)").cdr.cdr.car)
+        assert_equal(:cons, Scheme::parse("(cons 1 2)").car)
+        assert_equal(:cons, Scheme::parse("(cons 1 (cons 1 2))").cdr.cdr.car.car)
+        assert_equal(:cons, Scheme::parse("((cons 1  2) (cons 1 2))").cdr.car.car)
 
-        assert_equal(Scheme::parse("(begin (define proc1 (lambda (x) (define proc2 (lambda (y) (+ x y))) (proc2 7))) (proc1 10))").class, Scheme::Pair)
+        assert_equal(Scheme::Pair, Scheme::parse("(begin (define proc1 (lambda (x) (define proc2 (lambda (y) (+ x y))) (proc2 7))) (proc1 10))").class)
     end
 end
