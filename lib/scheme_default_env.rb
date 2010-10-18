ff97cb0e2466b283070343528ffb6205b779418b
svetlins <svetlin.s@gmail.com>
2010-10-19 00:45:47 +0300

    renamed default_env to global_env

--- lib/scheme_default_env.rb
+++ lib/scheme_default_env.rb
@@ -1,13 +1,44 @@
 module Scheme
-        @default_env = Environment.new
+        @global_env = Environment.new
 
-        #populate default env
-        @default_env.set(:+, NativeProcedure.new(lambda { |_| _.car + _.cdr.car }))
-        @default_env.set(:*, NativeProcedure.new(lambda { |_| _.car * _.cdr.car }))
-        @default_env.set(:-, NativeProcedure.new(lambda { |_| _.car - _.cdr.car }))
-        @default_env.set(:'=', NativeProcedure.new(lambda { |_| _.car == _.cdr.car }))
-        @default_env.set(:cons, NativeProcedure.new(lambda { |_| Pair.new(_.car, _.cdr.car) }))
-        @default_env.set(:car, NativeProcedure.new(lambda { |_| _.car.car }))
-        @default_env.set(:cdr, NativeProcedure.new(lambda { |_| _.car.cdr }))
-        @default_env.set(:null?, NativeProcedure.new(lambda { |_| _.car.nil? }))
+        #populate global environment with built-in procedures
+        @global_env.set(
+            :+,
+            NativeProcedure.new(lambda { |_| _.car + _.cdr.car })
+        )
+
+        @global_env.set(
+            :*,
+            NativeProcedure.new(lambda { |_| _.car * _.cdr.car })
+        )
+
+        @global_env.set(
+            :-,
+            NativeProcedure.new(lambda { |_| _.car - _.cdr.car })
+        )
+
+        @global_env.set(
+            :'=',
+            NativeProcedure.new(lambda { |_| _.car == _.cdr.car })
+        )
+
+        @global_env.set(
+            :cons,
+            NativeProcedure.new(lambda { |_| Pair.new(_.car, _.cdr.car) })
+        )
+
+        @global_env.set(
+            :car,
+            NativeProcedure.new(lambda { |_| _.car.car })
+        )
+
+        @global_env.set(
+            :cdr,
+            NativeProcedure.new(lambda { |_| _.car.cdr })
+        )
+
+        @global_env.set(
+            :null?,
+            NativeProcedure.new(lambda { |_| _.car.nil? })
+        )
 end
