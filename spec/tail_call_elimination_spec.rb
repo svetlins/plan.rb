753e34a2770e21d75723a5f6fd66f935fbe2dfc5
Svetlin Simonyan <svetlin.s@gmail.com>
2011-02-05 18:34:14 +0200

    Add tail call elimination spec

---
+++ spec/tail_call_elimination_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe "tail call elimination" do
+  def run(code)
+    Scheme.run code
+  end
+
+  it "makes possible arbitrary deep call stacks for tail calling procs" do
+    run("(begin
+           (define 
+             proc 
+             (lambda (x) 
+               (if (= x 2000)
+                 x 
+                 (proc  (+ x 1)))))
+           (proc 1))").should == 2000
+
+    run("(begin
+           (define 
+             proc 
+             (lambda (x) 
+               (if (= x 2000)
+                 42 
+                 (proc  (+ x 1)))))
+           (proc 1))").should == 42
+  end
+
+  it "fails on deepish call stacks otherwise" do
+    lambda do 
+      run("(begin
+             (define 
+               proc 
+               (lambda (x) 
+                 (if (= x 2000)
+                   42 
+                   (+ 1 (proc (+ x 1))))))
+             (proc 2))")
+    end.should raise_error(SystemStackError)
+    #end.call
+  end
+end
