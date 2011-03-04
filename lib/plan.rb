06a0bd0d1c27618cfee7f69c3b323580153c77eb
Svetlin Simonyan <svetlin.s@gmail.com>
2011-03-04 04:02:04 +0200

    Fix indentation

--- lib/plan.rb
+++ lib/plan.rb
@@ -5,51 +5,51 @@ require 'plan/globals'
 require 'plan/expression'
 
 module Plan
-    def self.run(code, env=nil)
-        # parse code
-        ast = Parser.new.parse code
+  def self.run(code, env=nil)
+    # parse code
+    ast = Parser.new.parse code
 
-        # apply any given bindings to global env
-        if env
-            current_env = @global_env.extend env
-        else
-            current_env = @global_env
+    # apply any given bindings to global env
+    if env
+      current_env = @global_env.extend env
+    else
+      current_env = @global_env
+    end
+
+    return Exp.new(ast).evaluate current_env
+  end
+
+  def self.shutdown
+    puts 'Bye!'
+    exit(0)
+  end
+
+  def self.repl
+    while true
+      begin
+        print "> "
+        # read 
+        code = readline
+        while code.count('(') != code.count(')') do
+          code += readline
         end
 
-        return Exp.new(ast).evaluate current_env
+        # eval
+        result = run code
+
+        # print
+        print '--> '
+        puts result.plan_inspect
+
+        #loop
+      rescue Interrupt
+        puts
+        puts 'keyboard interrupt'
+      rescue EOFError
+        shutdown
+      rescue StandardError => e
+        puts "!-> #{e}"
+      end
     end
-
-    def self.shutdown
-      puts 'Bye!'
-      exit(0)
-    end
-
-    def self.repl
-        while true
-            begin
-              print "> "
-              # read 
-              code = readline
-              while code.count('(') != code.count(')') do
-                  code += readline
-              end
-
-              # eval
-              result = run code
-
-              # print
-              print '--> '
-              puts result.plan_inspect
-
-              #loop
-            rescue Interrupt
-              puts
-              puts 'keyboard interrupt'
-            rescue EOFError
-              shutdown
-            rescue StandardError => e
-              puts "!-> #{e}"
-            end
-        end
-    end
+  end
 end
